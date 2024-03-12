ec76d86d04e5aee17658f470c04fbfca260cf024
Masoud <shikaozymandias@gmail.com>
2024-03-12 12:30:21 +0330

    update vite.config

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,8 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import eslint from "vite-plugin-eslint";
+// import eslint from "vite-plugin-eslint";
 
 export default defineConfig({
-  plugins: [react(), eslint()],
+  plugins: [react()],
   base: "/recipino",
 });
